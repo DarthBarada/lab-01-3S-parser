cc8c070b3ea5575cb8cb840024ffadb5300af3bf
DarthBarada <DarthBarada@gmail.com>
2019-10-27 17:59:45 +0300

    Little changes

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,7 +4,7 @@
 int main()
 	{
 		//Json json("{\"menu\": \"Файл\",\"commands\":[{\"title\":\"Новый\",\"action\":\"CreateDoc\"},{\"title\":\"Открыть\",\"action\":\"OpenDoc\"},{\"title\":\"Закрыть\",\"action\": \"CloseDoc\"}]}");
-		Json json("Json.json");
+		Json json("TestJson.json");
 		//Json json(R"({"lastname": "Ivanov","firstname": "Ivan","age": 25,"islegal": false,"marks":[4,5,5,5,2,3],"address":{"city":"Moscow","street": "Vozdvijenka"}})");
 		json.print();
 		//std::cout << "//---------------------------------\n";
